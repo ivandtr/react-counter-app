ca9c66fa17a68ab55e952724a893680e2a442358
idtr <ivanlozmar@gmail.com>
2020-04-24 22:33:52 +0200

    First implementation of Counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
     tags: ['tag1', 'tag2', 'tag3'],
   };
 
@@ -17,11 +17,6 @@ class Counter extends Component {
         <button onClick={this.handleClick} className="btn btn-secondary btn-sm">
           Increment
         </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
       </div>
     );
   }
