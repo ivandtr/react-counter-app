2b7b923e204ec8c920db9901e7a188496359efa9
idtr <ivanlozmar@gmail.com>
2020-04-25 21:32:15 +0200

    Refactor to remove state from Counter component

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,18 +4,36 @@ import Counter from './counter';
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
-      { id: 3, value: 0 },
+      { id: 3, value: 1 },
       { id: 4, value: 0 },
     ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          />
         ))}
       </div>
     );
