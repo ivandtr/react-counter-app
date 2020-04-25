efe82b97ac031e122081cfb6e5cd582b842dff51
idtr <ivanlozmar@gmail.com>
2020-04-26 00:29:14 +0200

    Refactor to lift the state up to App

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar{' '}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
